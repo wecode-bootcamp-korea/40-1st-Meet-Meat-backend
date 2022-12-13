731615b4918ecc0de27bcab9b140e5b8980a6ea4
정재헌 <jeongjaeheon@jeongjaeheon-ui-MacBookAir.local>
2022-12-13 11:55:20 +0900

    ADD:productDao 저장

--- api/models/productDao.js
+++ api/models/productDao.js
@@ -3,14 +3,20 @@ const AppData = require('./dataSource')
 const addCart = async(userId, productName) => {
     const data = [];
     
+    const ordersData = await AppData.query(
+        `INSERT INTO orders(
+            customers_id,
+        )`
+    )
+        
     const porkData = await AppData.query(
         `INSERT INTO order_itmes(
             orders_id,
             products_id,
             total_price_with_point,
-            total_quantity)
-            VALUES (?, ?, ?, ?)
-            `, []
+            total_quantity
+            ) VALUES (?, ?, ?, ?)
+            `, [userId, ]
         )
     const beefData = await AppData.query(
         `np`
