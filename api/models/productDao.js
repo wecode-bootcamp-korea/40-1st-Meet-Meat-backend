f59325b62b2aa05370fb2fc9a74e4533e0056090
정재헌 <jeongjaeheon@jeongjaeheon-ui-MacBookAir.local>
2022-12-14 21:27:37 +0900

    MODIFY:query 내 column명 변경

--- api/models/productDao.js
+++ api/models/productDao.js
@@ -9,7 +9,7 @@ const getAllProducts = async () => {
              description,
              date,
              tag_id,
-             categories_id,
+             category_id,
              products_size_id,
              products_type_id
         FROM products`)
@@ -26,7 +26,7 @@ const getProductsDataBycategoryId = async(categories_id) => {
             p.description,
             p.date,
             p.tag_id,
-            p.categories_id,
+            p.category_id,
             p.products_options_id 
           FROM products AS p
             WHERE p.categories_id =?`, [categories_id]
