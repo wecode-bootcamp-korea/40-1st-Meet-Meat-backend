49e580629e05cc9dea1713118e1a91ee9f8c43b3
hjcod <hjjeon5435@gmail.com>
2022-12-14 21:38:12 +0900

    [Modify]payment(point)차감 API 재구현

--- api/models/paymentDao.js
+++ api/models/paymentDao.js
@@ -1,17 +1,43 @@
+const { SimpleConsoleLogger } = require('typeorm');
 const AppData = require('./dataSource');
 
 const getTotalPayment = async (userId) =>{
     const result = await AppData.query(
       `
-      SELECT orders.customers_id, total_price 
-      FROM order_items 
+      SELECT
+        customers_id,
+        sum(total_price) as sum
+      FROM order_items
       JOIN orders
-      ON orders_id=orders.id
-      WHERE customers_id = ?
+      ON order_items.orders_id = orders.id
+      WHERE customers_id = ? 
       `,[userId]
     )
-        return result;
+    
+    const getPointByUserId = await AppData.query(
+      `
+      SELECT point
+      FROM customers
+      WHERE id =?
+      `,[userId]
+    )
+    const sumpoint = getPointByUserId[0].point - result[0].sum
+    
+    await AppData.query(
+      `UPDATE customers
+      SET point = ?
+      WHERE id = ?
+      `,[sumpoint,userId]
+    )
+
+    return sumpoint;
+
 }
 
 
+
 module.exports = { getTotalPayment }
+
+
+
+
--- api/services/paymentService.js
+++ api/services/paymentService.js
@@ -2,14 +2,9 @@ const { paymentDao } = require('../models')
 
 
 const getPaymentsByUser = async (user) => {
-// console.log(user)  
-//   id: 31,
-//   name: 'ABCCCCC',
-//   email: 'CIBA1234@email.com',
-//   password: '$2b$10$HLPBr7LfbjvTAS7jcxFo9uheq6k4ioGZ8cZ1E9CkstP8RC5AVjdY.'
 	const userId = user.id 
-	const payments = await paymentDao.getTotalPayment(userId)
-	return payments
+	const userPointLeft = await paymentDao.getTotalPayment(userId)
+	return {userPointLeft}
 }
 
 module.exports = { getPaymentsByUser }
